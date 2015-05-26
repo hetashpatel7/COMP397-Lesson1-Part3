62fff25b03431afb270758a87af09e4fb113416b
hetashpatel7 <hetashpatel7@gmail.com>
2015-05-26 14:44:30 -0400

    added createjs structure

--- COMP397-Lesson1-Part3/scripts/game.ts
+++ COMP397-Lesson1-Part3/scripts/game.ts
@@ -1,2 +1,25 @@
-﻿function init()
-{ console.log("Game is running"); } 
+﻿/// <reference path="typings/easeljs/easeljs.d.ts" />
+var stage:createjs.Stage;
+var canvas = document.getElementById("canvas");
+function init()
+{
+    stage = new createjs.Stage(canvas);
+        createjs.Ticker.setFPS(60);
+    createjs.Ticker.on("tick", gameLoop);
+    main();
+} 
+function gameLoop()
+{
+    stage.update();
+
+
+}
+function main()
+{
+    console.log("game is running...");
+
+    var helloText: createjs.Text = new createjs.Text("Hello World!", "40px consolas", "#0000000");
+    helloText.x = 30;
+    helloText.y = 200;
+    stage.addChild(helloText)
+}
